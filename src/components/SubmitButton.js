e8a8f6ad98cf0ea0e13c41f15372a7e0797209ff
Galen <galen.h.white@gmail.com>
2018-01-13 18:30:29 -0500

    Added a Submit Button and Custom Error Messages

---
+++ src/components/SubmitButton.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const SubmitButton = props => {
+
+  return(
+    <input
+      className='button'
+      type='submit'
+      value='Submit'
+    />
+  )
+}
+
+export default SubmitButton
---
+++ src/constants/ErrorMessages.js
@@ -0,0 +1,11 @@
+let errorMessages = {
+  cardName: "What is the name of your card?",
+  cardText: "Draw a card? Double your gold? You could do anything!",
+  cardCost: "Nothing is free.",
+  cardImageUrl: "Please include an image.",
+  potions: "Did you want to require potions?",
+  title: "No one wants to be a peasant...",
+  firstName: "You have a name, right?",
+  lastName: "Don't forget a last name!"
+}
+export default errorMessages
