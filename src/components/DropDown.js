a5e122ed6fb1dab0198d26851371655df9204552
Galen <galen.h.white@gmail.com>
2018-01-20 11:57:06 -0500

    Renamed to be more re-useable

---
+++ src/components/DropDown.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const DropDown = props => {
+
+  let handleClick = (event) => {
+    let value = event.target.value
+    props.handleClick(value)
+  }
+
+  let optionTags = props.options.map((value) => {
+    return (
+      <option key={value} value={value}>{value}</option>
+    )
+  })
+
+  return(
+    <div>
+      <label>{props.label}</label>
+      <select onChange={handleClick}>
+        <option defaultValue=""></option>
+        {optionTags}
+      </select>
+    </div>
+  )
+}
+
+export default DropDown
