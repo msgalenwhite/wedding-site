850ca0578066743f6e88e273fa4c713aee29a102
Galen <galen.h.white@gmail.com>
2018-01-22 19:42:12 -0500

    Card Image Sources

---
+++ src/constants/CardImages.js
@@ -0,0 +1,19 @@
+//image url's for Cards
+let cardImages = {
+  action: "https://imgur.com/jUq8A3n.png",
+  victory: "https://imgur.com/w9aZmRz.png",
+
+  //for duration, the cost is very off, the rest only a little off
+  duration: "https://imgur.com/CS5QEnk.png",
+
+  //everything is shifted right a little
+  reaction: "https://imgur.com/y8ILU5B.png",
+
+  //everythign is slightly off
+  victoryAndTreasure: "https://imgur.com/kNXsGbh.png",
+
+  //things are slighly too high
+  actionAndVictory: "https://imgur.com/03TnbIC.png"
+}
+
+export default cardImages
