88027c606bcdf3335918bc7182feabd043fff50d
Galen <galen.h.white@gmail.com>
2018-02-17 20:16:54 -0800

    testing

--- server.rb
+++ server.rb
@@ -86,25 +86,25 @@ end
 #   json userNameObj
 # end
 get "/testemail" do
-
-  from = Email.new(email: 'test@example.com')
-  subject = 'Hello World from the SendGrid Ruby Library!'
-  to = Email.new(email: 'galen.h.white@gmail.com')
-  content = Content.new(type: 'text/plain', value: 'Hello, Email!')
-  mail = Mail.new(from, subject, to, content)
-
-  sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
-
-  #here we're actually sending the email
-  response = sg.client.mail._('send').post(request_body: mail.to_json)
-
-  puts "======="
-
-  puts response.status_code
-  puts response.body
-  puts response.headers
+  #
+  # from = Email.new(email: 'test@example.com')
+  # subject = 'Hello World from the SendGrid Ruby Library!'
+  # to = Email.new(email: 'galen.h.white@gmail.com')
+  # content = Content.new(type: 'text/plain', value: 'Hello, Email!')
+  # mail = Mail.new(from, subject, to, content)
+  #
+  # sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+  #
+  # #here we're actually sending the email
+  # response = sg.client.mail._('send').post(request_body: mail.to_json)
 
   puts "======="
+
+  # puts response.status_code
+  # puts response.body
+  # puts response.headers
+  #
+  # puts "======="
 
 end
 
