37334895450a4fd7f18a50e457a9bbb7c5d57b1f
Galen <galen.h.white@gmail.com>
2018-02-25 18:36:22 -0500

    Changed RSVP Entry from a container to a component

---
+++ src/components/RsvpEntry.js
@@ -0,0 +1,48 @@
+import React from 'react'
+
+const RsvpEntry = props => {
+
+  let yesBoxStyle;
+  let noBoxStyle;
+
+  if (props.attending) {
+    yesBoxStyle = "check fa fa-check-square"
+    noBoxStyle = "check fa fa-square"
+  } else {
+    yesBoxStyle = "check fa fa-square"
+    noBoxStyle = "check fa fa-check-square"
+  }
+
+  let check = <i className="fa fa-check"></i>
+
+  let attending = <i
+    className={`${yesBoxStyle} attending`}
+    onClick={props.yesClick}
+  >
+  </i>
+
+  let notAttending = <i
+    className={`${noBoxStyle} notAttending`}
+    onClick={props.noClick}
+  >
+  </i>
+
+
+
+  return(
+    <div>
+      <h2>{props.name}</h2>
+
+      <span>
+        {attending}
+        <label>Will Attend</label>
+      </span>
+      <span>
+        {notAttending}
+        <label>Will Not Attend</label>
+      </span>
+    </div>
+  )
+}
+
+export default RsvpEntry
