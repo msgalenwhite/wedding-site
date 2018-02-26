ca9356c399749c6a310f826fd4ae1fef8977222f
Galen <galen.h.white@gmail.com>
2018-02-26 13:12:58 -0500

    Guests can include toasts/stories

---
+++ src/containers/StoryForm.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const StoryForm = props => {
+
+  //there will be a blurb about toasts/stories
+
+  //there will be a single text area, with a "tell another" button
+    //tell another will make another story block appear
+  //there will be a "bride/groom/both" toggle for topics
+
+  //there will be a place to enter an email
+
+  //there will be a final submit button
+  //maybe this button takes you to a summary page?
+
+  //maybe a back button to change their RSVP?
+  //should there be a blurb to show what their RSVP was?
+    //make sure to account for Plus Ones being "tbd"
+  return(
+    <h3>howdy</h3>
+  )
+}
+
+export default StoryForm
