b6d9f77ecd2d46db900a441485279f8abad40c4f
Galen <galen.h.white@gmail.com>
2018-03-02 15:15:19 -0500

    RSVP Functionality will go live July 1st

---
+++ src/components/DummyRSVP.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const DummyRSVP = props => {
+
+  return(
+    <div className='page'>
+      Please check back after July 1st!
+    </div>
+  )
+}
+
+export default DummyRSVP
