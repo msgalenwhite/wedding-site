8af71b8c1bc066f2f273a74d4b501348c7cddde0
Galen <galen.h.white@gmail.com>
2018-03-05 11:30:15 -0500

    Formatted DummySite

--- app/src/components/DummyRSVP.js
+++ app/src/components/DummyRSVP.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import {Link} from 'react-router'
 
 const DummyRSVP = props => {
 
   return(
-    <div className='page'>
-      Please check back after July 1st!
+    <div className='page dummy'>
+      <p>Galen and Chris would love to see you at their wedding, and are so glad you decided to RSVP as soon as possible!</p>
+      <p>Unfortunately, they are unable to accept any RSVPs before July 1st.</p>
+      <p>Please check back soon!</p>
+      <Link to='/'>
+        <button className='button'>
+          Return to Main
+        </button>
+      </Link>
     </div>
   )
 }
